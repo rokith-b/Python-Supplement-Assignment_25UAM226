3fffba9075364e9797eb1ff26cd70825e3450964
Rokith <rokith.b_25am@kgkite.ac.in>
2026-01-30 15:08:37 +0530

    problems_solved

--- problem_37.py
+++ problem_37.py
@@ -2,7 +2,7 @@
 # Find and fix the error
 
 def is_leap_year(year):
-    if year % 4 == 0:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     else:
         return False
